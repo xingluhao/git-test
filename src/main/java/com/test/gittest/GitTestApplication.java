9971e7e81e2640da1193dfc8ddf8e458b342a446
xing <2621935866@qq.com>
2022-08-27 21:01:08 +0800

    gitee commit

--- src/main/java/com/test/gittest/GitTestApplication.java
+++ src/main/java/com/test/gittest/GitTestApplication.java
@@ -14,6 +14,7 @@ public class GitTestApplication {
         System.out.println("hot-fix-test");
         System.out.println("push-test");
          System.out.println("push-test1111");
+         System.out.println("gitee-test1111");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
