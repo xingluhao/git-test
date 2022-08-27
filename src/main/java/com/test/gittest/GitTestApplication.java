66ac73050ccd9eba918450d6b9ab7c02bfa3e7cf
xing <2621935866@qq.com>
2022-08-27 16:13:08 +0800

    hot-fix commit 1

--- src/main/java/com/test/gittest/GitTestApplication.java
+++ src/main/java/com/test/gittest/GitTestApplication.java
@@ -9,6 +9,7 @@ public class GitTestApplication {
     public static void main(String[] args) {
         System.out.println("git2");
         System.out.println("git3");
+        System.out.println("git4 hot-fix");
         SpringApplication.run(GitTestApplication.class, args);
     }
 
